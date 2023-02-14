f0493cadd71706f00b6309b05fa3fb8a5338df19
Tiffany Lee <tl5110@g.rit.edu>
2023-02-13 22:38:18 -0500

    Created, started writing code, and doc-stringed Subtract class

---
+++ src/machine/instructions/Subtract.java
@@ -0,0 +1,42 @@
+package machine.instructions;
+
+import machine.InstructionStack;
+import machine.Maquina;
+
+/**
+ * The SUBTRACT instruction
+ *
+ * @author Tiffany Lee
+ */
+public class Subtract implements Instruction{
+    /** the instruction stack */
+    private final InstructionStack stack;
+
+    /**
+     * Create a new instruction.
+     * @param machine the machine
+     */
+    public Subtract(Maquina machine) {
+        this.stack = machine.getInstructionStack();
+    }
+
+    /**
+     * Pops the second and then first operands off the stack, and pushes
+     * the result of the first subtracted by the second
+     */
+    @Override
+    public void execute() {
+        // TODO
+        stack.push(stack.pop() - stack.pop());
+    }
+
+    /**
+     * Show the instruction using text so that it can be understood by a person.
+     *
+     * @return a short string describing what this instruction will do
+     */
+    @Override
+    public String toString() {
+        return Maquina.SUBTRACT;
+    }
+}
