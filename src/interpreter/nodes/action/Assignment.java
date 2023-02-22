58bc7d0692e6d03ba005a016b1acc85a04cf4b44
Tiffany Lee <tl5110@g.rit.edu>
2023-02-22 18:41:09 -0500

    Implemented and tested Action Classes: Assignment and Print

--- src/interpreter/nodes/action/Assignment.java
+++ src/interpreter/nodes/action/Assignment.java
@@ -4,24 +4,58 @@ import common.SymbolTable;
 import interpreter.nodes.expression.ExpressionNode;
 import java.io.PrintWriter;
 
+/**
+ * An ActionNode that represents the assignment of the value of
+ * an expression to a variable
+ *
+ * @author Tiffany Lee
+ */
 public class Assignment implements ActionNode {
+    /** the name of the variable */
     private final String name;
+    /** the child expression */
     private final ExpressionNode child;
 
+    /**
+     * Creates a new Assignment node with an identifier name and child
+     * expression
+     *
+     * @param name the name fo the variable that is getting a new value
+     * @param child the expression on the right-hand-side (RHS) of the
+     *              assignment statement
+     */
     public Assignment(String name, ExpressionNode child) {
         this.name = name;
         this.child = child;
     }
 
+    /**
+     * Print to standard output the assignment with the variable name,
+     * followed by the assignment token, and followed by the infix form
+     * of the child expression
+     */
     public void emit(){
-        // TODO
+        System.out.print(name + " = ");
+        child.emit();
     }
 
+    /**
+     * Evaluates the child expression and assigns the result to the variable
+     *
+     * @param symbol the table where variable values are stored
+     */
     public void execute(SymbolTable symbol){
-        // TODO
+        symbol.set(name, child.evaluate(symbol));
     }
 
+    /**
+     * Generates the MAQ instructions that when instructed will perform
+     * the assignment
+     *
+     * @param out the stream to write output to using out.println()
+     */
     public void compile(PrintWriter out){
-        // TODO
+        child.compile(out);
+        out.println("STORE " + name);
     }
 }
--- src/interpreter/nodes/action/Print.java
+++ src/interpreter/nodes/action/Print.java
@@ -4,22 +4,51 @@ import common.SymbolTable;
 import interpreter.nodes.expression.ExpressionNode;
 import java.io.PrintWriter;
 
+/**
+ * A node that represents the displaying of the value of an expression
+ *
+ * @author Tiffany Lee
+ */
 public class Print implements ActionNode{
+    /** the child expression */
     private final ExpressionNode child;
 
+    /**
+     * Creates a new Print node
+     *
+     * @param child the child expression
+     */
     public Print(ExpressionNode child) {
         this.child = child;
     }
 
+    /**
+     * Prints teh statement to standard output in the format "Print"
+     * followed by the infix form of the expression
+     */
     public void emit(){
-        // TODO
+        System.out.print("Print ");
+        child.emit();
     }
 
+    /**
+     * Evaluates the child expression and prints the result to
+     * standard output
+     *
+     * @param symbol the table where variable values are stored
+     */
     public void execute(SymbolTable symbol){
-        // TODO
+        System.out.println(child.evaluate(symbol));
     }
 
+    /**
+     * Generates the MAQ instructions that when instructed will perform
+     * the print action
+     *
+     * @param out the stream to write output to using out.println()
+     */
     public void compile(PrintWriter out){
-        // TODO
+        child.compile(out);
+        out.println("PRINT");
     }
 }
